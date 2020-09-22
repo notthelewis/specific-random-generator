731b9cbf19ee66588abe6235c81cfcaeb3769800
Lewis <43108320+notthelewis@users.noreply.github.com>
2020-09-22 01:18:00 +0100

    Create 102_random_3digit_ints.c

---
+++ 102_random_3digit_ints.c
@@ -0,0 +1,36 @@
+/*
+	C program to generate 102 random numbers which are exactly 3 characters long
+*/
+
+#include <stdio.h>
+
+int xorshift(int x) {
+	x ^= x << 13;
+	x ^= x >> 17; 
+	x ^= x << 5;
+	return x;
+}
+
+int main(int argc, char const *argv[])
+{
+
+	int i, j, x, y;
+
+	// g keeps count of current number being generated, for computation with current number
+	int g = 0;
+
+	for (i=16; i<=21; i++) {
+		x = xorshift(i);
+		y = (x >> 0x0F);
+		printf("%d\n", y);
+		g++;
+		for (j=31; j>=16; j--) {
+			x = xorshift(j+i);
+			y = (x >> 0x0F);
+			printf("%d\n", y);
+			g++;
+		}
+	}
+	printf("Generated %d \"random\" numbers  which are all 3 characters long\n ",g);
+	return 0;
+}
